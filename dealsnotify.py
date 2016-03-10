8c49a397cbf24e90b7fea20ea6dc5e48ad9533e4
Mancuerna <email-blocked>
2016-03-10 17:20:27 +0100

    Added rotating logs.

--- dealsnotify.py
+++ dealsnotify.py
@@ -2,17 +2,21 @@ import utility
 import time
 import logging
 import telebot
-
+from logging.handlers import RotatingFileHandler
 
 bot = telebot.TeleBot("TOKEN")
 
 
 def main():
     FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(filename='dealsnotify.log',
-                        level=logging.INFO, format=FORMAT)
+    logging.basicConfig(level=logging.INFO, format=FORMAT)
     logging.getLogger("requests").setLevel(logging.WARNING)
     logging.getLogger("urllib3").setLevel(logging.WARNING)
+    handler = RotatingFileHandler(filename='dealsnotify.log', maxBytes=1048576,
+                                  backupCount=5)
+    fmt = logging.Formatter(FORMAT)
+    handler.setFormatter(fmt)
+    logging.getLogger('').addHandler(handler)
     logging.info('Started')
     # utility.get_threads("ofertas")
     utility.scan_first_page("ofertas", bot)
